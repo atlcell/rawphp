4667b57cb193925ab4e5b27287f1dcf053fd71e6
atlcell <hello@atlcell.com>
2022-01-28 13:13:38 -0500

    tests and screens

---
+++ tests/test-basic.php
@@ -0,0 +1,39 @@
+<?php 
+declare(strict_types=1);
+require __DIR__ . '/../vendor/autoload.php';
+
+
+
+use PHPUnit\Framework\TestCase;
+
+final class StackTest extends TestCase
+{
+    public function testEmpty(): array
+    {
+        $stack = [];
+        $this->assertEmpty($stack);
+
+        return $stack;
+    }
+
+    /**
+     * @depends testEmpty
+     */
+    public function testPush(array $stack): array
+    {
+        array_push($stack, 'foo');
+        $this->assertSame('foo', $stack[count($stack)-1]);
+        $this->assertNotEmpty($stack);
+
+        return $stack;
+    }
+
+    /**
+     * @depends testPush
+     */
+    public function testPop(array $stack): void
+    {
+        $this->assertSame('foo', array_pop($stack));
+        $this->assertEmpty($stack);
+    }
+}
